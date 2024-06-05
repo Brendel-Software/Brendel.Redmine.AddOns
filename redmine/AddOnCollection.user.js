2ecfcd1d3c0d4fe5070dff6fa9ff09785ae7c0f6
rolschewsky@brendel-software.de <rolschewsky@brendel-software.de>
2024-06-05 12:34:08 +0200

    refactor: Files umbenannt

--- redmine/AddOnCollection.user.js
+++ redmine/AddOnCollection.user.js
@@ -1,15 +1,15 @@
 // ==UserScript==
-// @name         Brendel Redmine AddOns
+// @name         Brendel Redmine AddOn Collection
 // @namespace    https://brendel.software/
 // @version      1.1
-// @description  Verbessert die Redmine
+// @description  Eine Sammlung an Remine AddOns
 // @author       R. Olschewsky
 // @match        https://redmine.brendel.software/*
 // @icon         https://github.com/Brendel-Software/Brendel.Tampermonkey/blob/master/icons/redmine.png?raw=true
 // @grant        none
 // @require      http://code.jquery.com/jquery-latest.js
-// @updateURL    https://raw.githubusercontent.com/Brendel-Software/Brendel.Tampermonkey/master/redmine/AddOns.meta.js
-// @downloadURL  https://raw.githubusercontent.com/Brendel-Software/Brendel.Tampermonkey/master/redmine/AddOns.user.js
+// @updateURL    https://raw.githubusercontent.com/Brendel-Software/Brendel.Tampermonkey/master/redmine/AddOnCollection.meta.js
+// @downloadURL  https://raw.githubusercontent.com/Brendel-Software/Brendel.Tampermonkey/master/redmine/AddOnCollection.user.js
 // ==/UserScript==
 /* globals $ */
 "use strict";
