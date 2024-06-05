228c174adcffaad54926351f49b2c35da4f15f6d
Rüdiger Alexander Olschewsky <ruediger.olschewsky@gmail.com>
2024-06-05 10:53:57 +0200

    Update and rename Zeiterfassung3.user.js to Zeiterfassung4.user.js

--- scripts/Zeiterfassung4.user.js
+++ scripts/Zeiterfassung4.user.js
@@ -1,15 +1,15 @@
 // ==UserScript==
 // @name         Redmine AddOn: Zeiterfassung
 // @namespace    https://brendel.software/
-// @version      1.1
+// @version      1.4
 // @description  Verbessert die Redmine Zeiterfassung
 // @author       R. Olschewsky
 // @match        https://redmine.brendel.software/issues/*/time_entries/*
 // @icon         https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200
 // @grant        none
 // @require      http://code.jquery.com/jquery-latest.js
-// @updateURL    https://raw.githubusercontent.com/Brendel-Software/Brendel.Redmine.AddOns/master/scripts/Zeiterfassung3.user.js
-// @downloadURL  https://raw.githubusercontent.com/Brendel-Software/Brendel.Redmine.AddOns/master/scripts/Zeiterfassung3.meta.js
+// @updateURL    https://raw.githubusercontent.com/Brendel-Software/Brendel.Redmine.AddOns/master/scripts/Zeiterfassung4.user.js
+// @downloadURL  https://raw.githubusercontent.com/Brendel-Software/Brendel.Redmine.AddOns/master/scripts/Zeiterfassung4.meta.js
 // ==/UserScript==
 /* globals $ */
 (function() {
